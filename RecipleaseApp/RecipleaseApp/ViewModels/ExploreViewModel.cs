74a66b442e990309abe910894b6d519d5618cdcf
User <User@LAB3-19>
2021-11-14 12:24:27 +0200

    explore page

---
+++ RecipleaseApp/RecipleaseApp/ViewModels/ExploreViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Essentials;
+using System.Windows.Input;
+using Xamarin.Forms;
+using RecipleaseApp.Services;
+using RecipleaseApp.Models;
+using RecipleaseApp.Views;
+
+namespace RecipleaseApp.ViewModels
+{
+    class ExploreViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
